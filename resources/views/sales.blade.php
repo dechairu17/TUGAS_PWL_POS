fbe4b4262030fd83c2f5634f09bca1e543e84def
dechairu17 <dechairu@gmail.com>
2025-02-28 13:03:11 +0700

    Commit

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.app')
 @section('content')
 <h2 class="text-center">Transaksi Penjualan</h2>
-<div class="text-center">
-    <p>Silakan pilih produk untuk melakukan transaksi penjualan.</p>
-</div>
 @endsection
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,8 +4,4 @@
 <h2>User Profile</h2>
 <p>ID: {{ $id }}</p>
 <p>Name: {{ $name }}</p>
-<form method="GET" action="/user/{{ $id }}/name/">
-    <input type="text" name="name" placeholder="Enter New Name">
-    <button type="submit" class="btn btn-primary">Change Name</button>
-</form>
 @endsection
